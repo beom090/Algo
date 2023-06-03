639187da54b3af4b14f456ed3ffee62fd1aeda67
Park Junbeom <108970338+beom090@users.noreply.github.com>
2023-06-03 11:40:45 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,9 @@
+import string
+def solution(s):
+    if len(s) == 4 or len(s) == 6:
+        if s.isdigit():
+            return True
+        else:
+            return False
+    else:
+        return False
